23ecdacb9aab608020cbb564bb96bdcfa4885048
Nafis Chonchol <145265617+chonchol-alzaf@users.noreply.github.com>
2025-03-03 12:37:18 +0600

    spelling fixed

--- app/Http/Controllers/WriterController.php
+++ app/Http/Controllers/WriterController.php
@@ -6,11 +6,34 @@ use App\Models\Writer;
 
 class WriterController extends Controller
 {
+    public function __construct(private writerInterface $writerItnterfafce)
+    
     public function index()
     {
+        $respones = $this->writerItnterfafce->index();
+
         $writers = Writer::with("posts")
         ->get();
         
         return $writers;
+    }
+}
+
+
+class writerOne implements writerInterface
+{
+    function index()
+    {
+        ///fsdfsd
+        return "one";
+    }
+}
+
+class writerTwo implements writerInterface
+{
+    function index()
+    {
+        //fsfsd
+        return "two";
     }
 }
